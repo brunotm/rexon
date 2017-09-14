002638514998d0b56decf78b490b73e2364f020a
Bruno Moura <brunotm@gmail.com>
2017-09-14 10:08:54 +0100

    Reduce allocations
    Reduce allocations by a factor of 2 in JSONSet
    Use strconv.Append* when possible to avoid string parsing
    Add a custom string marshaling function to avoid using encoding/json when surrogates are found or escaping is needed.

--- json.go
+++ json.go
@@ -2,7 +2,9 @@ package rexon
 
 import (
 	"encoding/json"
+	"reflect"
 	"strconv"
+	"unicode/utf8"
 	"unsafe"
 
 	"github.com/brunotm/jsonparser"
@@ -81,9 +83,8 @@ func JSONSetRawBytes(data []byte, value []byte, path ...string) ([]byte, error) 
 // JSONSet parses and set the given value for the provided path.
 // byte slices will be set as string, value is already a parsed type use JSONSetRawBytes instead
 func JSONSet(data []byte, value interface{}, path ...string) ([]byte, error) {
-	var res []byte
 	var err error
-	var str string
+	buf := make([]byte, 0, 8)
 
 	// Creates a JSON if we're given a nil or empty []byte
 	if len(data) == 0 {
@@ -93,63 +94,45 @@ func JSONSet(data []byte, value interface{}, path ...string) ([]byte, error) {
 
 	switch v := value.(type) {
 	case []byte:
-		sz := len(v) + 2
-		res = make([]byte, sz, sz)
-		res = append(res, '"')
-		res = append(res, v...)
-		res = append(res, '"')
-		return JSONSetRawBytes(data, res, path...)
+		// v = bytes.TrimSpace(v)
+		// buf = parseString(*(*string)(unsafe.Pointer(&v)), false)
+		buf = marshalByteString(v, false)
 	case string:
-		if mustMarshalString(v) {
-			res, _ = json.Marshal(v)
-			return JSONSetRawBytes(data, res, path...)
-		}
-		str = `"` + v + `"`
+		shdr := *(*reflect.StringHeader)(unsafe.Pointer(&v))
+		bhdr := reflect.SliceHeader{Data: shdr.Data, Len: shdr.Len, Cap: shdr.Len}
+		bv := *(*[]byte)(unsafe.Pointer(&bhdr))
+		buf = marshalByteString(bv, false)
 	case bool:
-		if v {
-			str = "true"
-		} else {
-			str = "false"
-		}
+		buf = strconv.AppendBool(buf, v)
 	case int:
-		str = strconv.FormatInt(int64(v), 10)
+		buf = strconv.AppendInt(buf, int64(v), 10)
 	case int8:
-		str = strconv.FormatInt(int64(v), 10)
+		buf = strconv.AppendInt(buf, int64(v), 10)
 	case int16:
-		str = strconv.FormatInt(int64(v), 10)
+		buf = strconv.AppendInt(buf, int64(v), 10)
 	case int32:
-		str = strconv.FormatInt(int64(v), 10)
+		buf = strconv.AppendInt(buf, int64(v), 10)
 	case int64:
-		str = strconv.FormatInt(v, 10)
+		buf = strconv.AppendInt(buf, v, 10)
 	case uint8:
-		str = strconv.FormatUint(uint64(v), 10)
+		buf = strconv.AppendUint(buf, uint64(v), 10)
 	case uint16:
-		str = strconv.FormatUint(uint64(v), 10)
+		buf = strconv.AppendUint(buf, uint64(v), 10)
 	case uint32:
-		str = strconv.FormatUint(uint64(v), 10)
+		buf = strconv.AppendUint(buf, uint64(v), 10)
 	case uint64:
-		str = strconv.FormatUint(v, 10)
+		buf = strconv.AppendUint(buf, v, 10)
 	case float32:
-		str = strconv.FormatFloat(float64(v), 'f', -1, 64)
+		buf = strconv.AppendFloat(buf, float64(v), 'f', -1, 64)
 	case float64:
-		str = strconv.FormatFloat(v, 'f', -1, 64)
+		buf = strconv.AppendFloat(buf, v, 'f', -1, 64)
 	default:
-		if res, err = json.Marshal(value); err != nil {
+		if buf, err = json.Marshal(value); err != nil {
 			return nil, err
 		}
-		return JSONSetRawBytes(data, res, path...)
 	}
 
-	// strHdr := (*reflect.StringHeader)(unsafe.Pointer(&str))
-	// byteHdr := (*reflect.SliceHeader)(unsafe.Pointer(&res))
-	// byteHdr.Data = strHdr.Data
-	// l := len(str)
-	// byteHdr.Len = l
-	// byteHdr.Cap = l
-
-	res = []byte(str)
-
-	return JSONSetRawBytes(data, res, path...)
+	return JSONSetRawBytes(data, buf, path...)
 }
 
 // CopyBytes copies a give []byte
@@ -159,11 +142,79 @@ func CopyBytes(data []byte) []byte {
 	return b
 }
 
-func mustMarshalString(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if s[i] < ' ' || s[i] > 0x7f || s[i] == '"' {
-			return true
+// String below serialization functionality adapted from encoding/json
+
+const chars = "0123456789abcdef"
+
+func marshalByteString(s []byte, noHTMLEscape bool) []byte {
+	buf := make([]byte, 1, len(s)+2)
+	buf[0] = '"'
+
+	p := 0 // last non-escape symbol
+
+	for i := 0; i < len(s); {
+		c := s[i]
+
+		if isNotEscapedSingleChar(c, !noHTMLEscape) {
+			// single-width character, no escaping is required
+			i++
+			continue
+		} else if c < utf8.RuneSelf {
+			// single-with character, need to escape
+			buf = append(buf, s[p:i]...)
+			switch c {
+			case '\t':
+				buf = append(buf, `\t`...)
+			case '\r':
+				buf = append(buf, `\r`...)
+			case '\n':
+				buf = append(buf, `\n`...)
+			case '\\':
+				buf = append(buf, `\\`...)
+			case '"':
+				buf = append(buf, `\"`...)
+			default:
+				buf = append(buf, `\u00`...)
+				buf = append(buf, chars[c>>4])
+				buf = append(buf, chars[c&0xf])
+			}
+
+			i++
+			p = i
+			continue
 		}
+
+		// broken utf
+		runeValue, runeWidth := utf8.DecodeRune(s[i:])
+		if runeValue == utf8.RuneError && runeWidth == 1 {
+			buf = append(buf, s[p:i]...)
+			buf = append(buf, `\ufffd`...)
+			i++
+			p = i
+			continue
+		}
+
+		// jsonp stuff - tab separator and line separator
+		if runeValue == '\u2028' || runeValue == '\u2029' {
+			buf = append(buf, s[p:i]...)
+			buf = append(buf, `\u202`...)
+			buf = append(buf, chars[runeValue&0xf])
+			i += runeWidth
+			p = i
+			continue
+		}
+		i += runeWidth
 	}
-	return false
+	buf = append(buf, s[p:]...)
+	buf = append(buf, '"')
+	return buf
+}
+
+func isNotEscapedSingleChar(c byte, escapeHTML bool) bool {
+	// Note: might make sense to use a table if there are more chars to escape. With 4 chars
+	// it benchmarks the same.
+	if escapeHTML {
+		return c != '<' && c != '>' && c != '&' && c != '\\' && c != '"' && c >= 0x20 && c < utf8.RuneSelf
+	}
+	return c != '\\' && c != '"' && c >= 0x20 && c < utf8.RuneSelf
 }
