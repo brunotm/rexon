4ca0bc31da95f608a997ebc5631eefce5fedbaf2
Bruno Moura <brunotm@gmail.com>
2017-11-23 23:12:41 +0000

    Use named returns

--- helper.go
+++ helper.go
@@ -12,39 +12,39 @@ var (
 )
 
 // RexCompile wraps regexp.Compile
-func RexCompile(rex string) (*regexp.Regexp, error) {
+func RexCompile(rex string) (rexc *regexp.Regexp, err error) {
 	return regexp.Compile(rex)
 }
 
 // RexMustCompile wraps regexp.Compile
-func RexMustCompile(rex string) *regexp.Regexp {
+func RexMustCompile(rex string) (rexc *regexp.Regexp) {
 	return regexp.MustCompile(rex)
 }
 
 // RexSetCompile compiles a RexSet map
-func RexSetCompile(set map[string]string) (map[string]*regexp.Regexp, error) {
-	rexSet := make(map[string]*regexp.Regexp)
+func RexSetCompile(set map[string]string) (rset map[string]*regexp.Regexp, err error) {
+	rset = make(map[string]*regexp.Regexp)
 	for key, value := range set {
 		rex, err := regexp.Compile(value)
 		if err != nil {
 			return nil, err
 		}
-		rexSet[key] = rex
+		rset[key] = rex
 	}
-	return rexSet, nil
+	return rset, nil
 }
 
 // RexSetMustCompile is like RexSetCompile but panics on error
-func RexSetMustCompile(set map[string]string) map[string]*regexp.Regexp {
-	rexSet, err := RexSetCompile(set)
+func RexSetMustCompile(set map[string]string) (rset map[string]*regexp.Regexp) {
+	rset, err := RexSetCompile(set)
 	if err != nil {
 		panic(err)
 	}
-	return rexSet
+	return rset
 }
 
 // wrapCtxSend wraps the sending to a channel with a context
-func wrapCtxSend(ctx context.Context, result Result, resultCh chan<- Result) bool {
+func wrapCtxSend(ctx context.Context, result Result, resultCh chan<- Result) (ok bool) {
 	select {
 	case <-ctx.Done():
 		return false
@@ -53,7 +53,7 @@ func wrapCtxSend(ctx context.Context, result Result, resultCh chan<- Result) boo
 	}
 }
 
-func getFieldType(field string, fieldTypes map[string]ValueType) (ValueType, bool) {
+func getFieldType(field string, fieldTypes map[string]ValueType) (valueType ValueType, exists bool) {
 
 	// Return the specified ValueType
 	if valueType, exists := fieldTypes[field]; exists {
